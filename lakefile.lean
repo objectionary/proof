1906ee5626624bf3751a86ee7e229bbd472f37a2
eyihluyc <theviolthevioletandthewine@gmail.com>
2023-12-28 00:39:53 -0500

    Update lakefile

--- lakefile.lean
+++ lakefile.lean
@@ -1,14 +1,14 @@
 import Lake
 open Lake DSL
 
-package «phi-calculus» {
-  -- add any package configuration options here
-}
+package «phi-calculus»
 
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git"
 
+require std from git "https://github.com/leanprover/std4" @ "main"
+
 @[default_target]
-lean_lib PhiCalculus {
-  -- add any library configuration options here
-}
+lean_lib PhiCalculus
+
+lean_lib Minimal
