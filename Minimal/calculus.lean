6f614fd42fe445dc61cc7be76c06db7bc52ba3a4
eyihluyc <theviolthevioletandthewine@gmail.com>
2023-12-28 00:34:32 -0500

    Add whnf and nf

--- Minimal/calculus.lean
+++ Minimal/calculus.lean
@@ -1,5 +1,6 @@
 set_option autoImplicit false
 
+@[reducible]
 def Attr := String
 
 mutual
@@ -17,7 +18,7 @@ open OptionalAttr
 open Term
 
 
-def mapObj : (Term -> Term) -> (List (Attr × OptionalAttr)) -> (List (Attr × OptionalAttr))
+def mapObj : (Term → Term) → (List (Attr × OptionalAttr)) → (List (Attr × OptionalAttr))
   := λ f o =>
   let f' := λ (attr_name, attr_body) =>
       match attr_body with
@@ -34,11 +35,11 @@ partial def incLocatorsFrom : Nat → Term → Term
     | obj o => (obj (mapObj (incLocatorsFrom (k+1)) o))
 
 
-partial def incLocators : Term -> Term
+partial def incLocators : Term → Term
   := incLocatorsFrom 0
 
 
-partial def substituteLocator : Int × Term -> Term -> Term
+partial def substituteLocator : Int × Term → Term → Term
   := λ (k, v) term => match term with
     | obj o => obj (mapObj (substituteLocator (k + 1, incLocators v)) o)
     | dot t a => dot (substituteLocator (k, v) t) a
@@ -48,14 +49,56 @@ partial def substituteLocator : Int × Term -> Term -> Term
       else if (n == k) then v
       else loc (n-1)
 
+-- def checkUniqueAttributes : (List (Attr × OptionalAttr)) → Bool
+--   | _ => true
 
--- partial def whnf : Term → Term
---   | loc n => loc n
---   | obj o => obj o
---   | app t a u => match (whnf t) with
---     | obj o => __
---     | t' => app t' a u
+def lookup : (List (Attr × OptionalAttr)) → Attr → Option OptionalAttr
+  := λ l a => match l with
+    | [] => none
+    | List.cons (name, term) tail =>
+        if (name == a) then term
+        else lookup tail a
 
--- partial def nf : Term → Term
--- | loc n => loc n
--- | ...
+def insert : (List (Attr × OptionalAttr)) → Attr → OptionalAttr → (List (Attr × OptionalAttr))
+  := λ l a u => match l with
+    | [] => []
+    | List.cons (name, term) tail =>
+        if (name == a) then List.cons (name, u) tail
+        else List.cons (name, term) (insert tail a u)
+
+
+partial def whnf : Term → Term
+  | loc n => loc n
+  | obj o => obj o
+  | app t a u => match (whnf t) with
+    | obj o => match lookup o a with
+      | none => app (obj o) a u
+      | some (attached _) => app (obj o) a u
+      | some void => obj (insert o a (attached (incLocators u)))
+    | t' => app t' a u
+  | dot t a => match (whnf t) with
+    | obj o => match lookup o a with
+      | some (attached u) => whnf (substituteLocator (0, obj o) u)
+      | some void => dot (obj o) a
+      | none => match lookup o "φ" with
+        | some _ => dot (dot (obj o) "φ") a
+        | none => dot (obj o) a
+    | t' => dot t' a
+
+partial def nf : Term → Term
+  | loc n => loc n
+  | obj o => obj (mapObj nf o)
+  | app t a u => match (whnf t) with
+    | obj o => match lookup o a with
+      | none => app (nf (obj o)) a (nf u)
+      | some (attached _) => app (nf (obj o)) a (nf u)
+      | some void => nf (obj (insert o a (attached (incLocators u))))
+    | t' => app (nf t') a (nf u)
+  | dot t a => match (whnf t) with
+    | obj o => match lookup o a with
+      | some (attached u) => nf (substituteLocator (0, obj o) u)
+      | some void => dot (nf (obj o)) a
+      | none => match lookup o "φ" with
+        | some _ => nf (dot (dot (obj o) "φ") a)
+        | none => dot (nf (obj o)) a
+    | t' => dot (nf t') a
