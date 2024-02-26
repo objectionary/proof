eb6f096d22e8ec683449f7fb3a5ee61ebb69fbfb
eyihluyc <theviolthevioletandthewine@gmail.com>
2024-02-25 20:36:01 -0500

    Update doc-gen before Std

--- lakefile.lean
+++ lakefile.lean
@@ -3,10 +3,10 @@ open Lake DSL
 
 package «phi-calculus»
 
-require std from git "https://github.com/leanprover/std4" @ "main"
-
 meta if get_config? env = some "dev" then -- dev is so not everyone has to build it
 require «doc-gen4» from git "https://github.com/leanprover/doc-gen4" @ "main"
+
+require std from git "https://github.com/leanprover/std4" @ "main"
 
 @[default_target]
 lean_lib PhiCalculus
