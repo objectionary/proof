fe6d20b4a4b94dbc7d3188b3a83f2c284a66f231
Anatolay <nihonvasserman228@gmail.com>
2024-08-10 17:43:44 +0300

    Add definition of parallel reduction

---
+++ Extended/Reduction/Parallel/Definition.lean
@@ -0,0 +1,189 @@
+import Extended.Term
+
+set_option autoImplicit false
+
+/-! ### Defition of φ-calculus parallel term reduction `⇛` -/
+
+open Term
+
+structure Ctx where
+  glob : Term
+  scope : Term
+
+mutual
+
+-- | tᵢ ⇛ tᵢ' for all i with ⟦ ... αᵢ ↦ tᵢ ... ⟧
+--   α's are given by `attrs`
+inductive Premise : {attrs : Attrs} → Ctx → Bindings attrs → Bindings attrs → Type where
+  | nil : {ctx : Ctx} → Premise ctx .nil .nil
+  | consVoid
+    : {ctx : Ctx}
+    → (attr : Attr)
+    → { attrs : Attrs }
+    → { bnds1 : Bindings attrs }
+    → { bnds2 : Bindings attrs }
+    → { not_in : attr ∉ attrs }
+    → Premise ctx bnds1 bnds2
+    → Premise ctx (.cons attr not_in none bnds1) (.cons attr not_in none bnds2)
+  | consAttached
+    : {ctx : Ctx}
+    → (attr : Attr)
+    → (t1 : Term)
+    → (t2 : Term)
+    → PReduce ctx t1 t2
+    → { attrs : Attrs }
+    → { bnds1 : Bindings attrs }
+    → { bnds2 : Bindings attrs }
+    → { not_in : attr ∉ attrs }
+    → Premise ctx bnds1 bnds2
+    → Premise ctx (.cons attr not_in (some t1) bnds1) (.cons attr not_in (some t2) bnds2)
+
+inductive PReduce : Ctx → Term → Term → Type where
+  -- Dispatch
+  | pr_dot
+    : { ρ : Option Term }
+    → { attrs : Attrs }
+    → { bnds : Bindings attrs }
+    → { ctx : Ctx }
+    → { attr : Attr }
+    → { t t_inner : Term }
+    → PReduce ctx t (obj ρ bnds)
+    → lookup ρ bnds attr = LookupRes.attached t_inner
+    → PReduce ctx (dot t attr) (substitute t_inner (obj ρ bnds))
+  | pr_φ
+    : { ρ : Option Term }
+    → { attrs : Attrs }
+    → { bnds : Bindings attrs }
+    → { ctx : Ctx }
+    → { attr : Attr }
+    → { t : Term }
+    → PReduce ctx t (obj ρ bnds)
+    → lookup ρ bnds attr = LookupRes.absent
+    → "φ" ∈ attrs
+    → PReduce ctx (dot t attr) (dot (dot (obj ρ bnds) "φ") attr)
+  | pr_stop
+    : { ρ : Option Term }
+    → { attrs : Attrs }
+    → { bnds : Bindings attrs }
+    → { ctx : Ctx }
+    → { attr : Attr }
+    → { t : Term }
+    → PReduce ctx t (obj ρ bnds)
+    → lookup ρ bnds attr = LookupRes.absent
+    → lookup ρ bnds "φ" = LookupRes.absent
+    → lookup ρ bnds "λ" = LookupRes.absent
+    → PReduce ctx (dot t attr) termination
+  -- Application
+  | pr_empty
+    : { ρ : Option Term }
+    → { attrs : Attrs }
+    → { bnds : Bindings attrs }
+    → { ctx : Ctx }
+    → { t : Term }
+    → PReduce ctx t (obj ρ bnds)
+    → PReduce ctx (app t Record.nil) (obj ρ bnds)
+  | pr_copy
+    : { ρ : Option Term }
+    → { attrs app_attrs : Attrs }
+    → { bnds : Bindings attrs }
+    → { app_record_tail : Record Term app_attrs }
+    → { attr : Attr }
+    → { not_in : attr ∉ app_attrs }
+    → { g s t u u' : Term }
+    → PReduce {glob := g, scope := s} t (obj ρ bnds)
+    → PReduce {glob := g, scope := s} u u'
+    → lookup ρ bnds attr = LookupRes.void
+    → PReduce
+        { glob := g, scope := s }
+        (app t (Record.cons attr not_in u app_record_tail))
+        (app (insert ρ bnds attr (substitute u' s)) app_record_tail)
+  | pr_over
+    : { ρ : Option Term }
+    → { attrs app_attrs : Attrs }
+    → { bnds : Bindings attrs }
+    → { app_record_tail : Record Term app_attrs }
+    → { attr : Attr }
+    → { not_in : attr ∉ app_attrs }
+    → { ctx : Ctx }
+    → { t t_inner u : Term }
+    → PReduce ctx t (obj ρ bnds)
+    → lookup ρ bnds attr = LookupRes.attached t_inner
+    → PReduce
+        ctx
+        (app (obj ρ bnds) (Record.cons attr not_in u app_record_tail))
+        termination
+  | pr_miss
+    : { ρ : Option Term }
+    → { attrs app_attrs : Attrs }
+    → { bnds : Bindings attrs }
+    → { app_record_tail : Record Term app_attrs }
+    → { attr : Attr }
+    → { not_in : attr ∉ app_attrs }
+    → { ctx : Ctx }
+    → { t u : Term }
+    → PReduce ctx t (obj ρ bnds)
+    → lookup ρ bnds attr = LookupRes.absent
+    → lookup ρ bnds "φ" = LookupRes.absent
+    → lookup ρ bnds "λ" = LookupRes.absent
+    → PReduce
+        ctx
+        (app t (Record.cons attr not_in u app_record_tail))
+        termination
+  -- Special terms
+  | pr_Φ
+    : { g s : Term}
+    → PReduce { glob := g, scope := s} glob g
+  | pr_ξ
+    : { g s : Term}
+    → PReduce { glob := g, scope := s} glob s
+  | pr_dd
+    : { ctx : Ctx }
+    → { attr : Attr }
+    → PReduce ctx (dot termination attr) termination
+  | pr_dc
+    : { app_attrs : Attrs }
+    → { record : Record Term app_attrs }
+    → { ctx : Ctx }
+    → PReduce ctx (app termination record) termination
+  -- Congruence
+  | pr_cong_app
+    : { ctx : Ctx }
+    → {t t' u u' : Term}
+    → {attr : Attr}
+    → {app_attrs : Attrs}
+    → {not_in : attr ∉ app_attrs}
+    → (app_bnds : Record Term app_attrs)
+    → PReduce ctx t t'
+    → PReduce ctx u u'
+    → PReduce
+        ctx
+        (app t (Record.cons attr not_in u app_bnds))
+        (app t' (Record.cons attr not_in u' app_bnds))
+  | pr_cong_dot
+    : { ctx : Ctx }
+    → {t t' : Term}
+    → {attr : Attr}
+    → PReduce ctx t t'
+    → PReduce ctx (dot t attr) (dot t' attr)
+  | pr_cong_obj
+    : { g l : Term}
+    → {ρ : Option Term}
+    → {attrs : Attrs}
+    → {bnds new_bnds : Bindings attrs}
+    → Premise {glob := glob, scope := obj ρ bnds} bnds new_bnds
+    → PReduce
+        {glob := g, scope := l}
+        (obj ρ bnds)
+        (obj ρ new_bnds)
+  -- Reflexive
+  | pr_termination_refl
+    : { ctx : Ctx }
+    → PReduce ctx termination termination
+  | pr_ξ_refl
+    : { ctx : Ctx }
+    → PReduce ctx this this
+  | pr_Φ_refl
+    : { ctx : Ctx }
+    → PReduce ctx glob glob
+
+end
